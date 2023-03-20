7c103c18713d037d5fdf1eeb8f529f748837fcee
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-03-20 23:17:56 +0100

    initial commit

---
+++ api/apicamp/main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+
+class Campaign(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    logo = CloudinaryField('Image', overwrite=True, format='jpg')
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self) -> str:
+        return self.title
